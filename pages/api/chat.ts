b8bfa5d77f0548cc4961cb17f15f2412012a395e
leason-wan <fadeneow@gmail.com>
2024-04-03 10:42:13 +0800

    chore: rm tiktoken

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,11 +1,7 @@
 import { ChatBody, Message } from '@/types/chat';
 import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
-import { getEncoding } from 'js-tiktoken';
-// import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
-// import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
-// // @ts-expect-error
-// import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
+// import { getEncoding } from 'js-tiktoken';
 
 export const config = {
   runtime: 'edge',
@@ -15,33 +11,26 @@ const handler = async (req: Request): Promise<Response> => {
   try {
     const { model, messages, key, prompt } = (await req.json()) as ChatBody;
 
-    // await init((imports) => WebAssembly.instantiate(wasm, imports));
-    // const encoding = new Tiktoken(
-    //   tiktokenModel.bpe_ranks,
-    //   tiktokenModel.special_tokens,
-    //   tiktokenModel.pat_str,
-    // );
-
-    const encoding = getEncoding('gpt2');
+    // const encoding = getEncoding('gpt2');
 
     let promptToSend = prompt;
     if (!promptToSend) {
       promptToSend = DEFAULT_SYSTEM_PROMPT;
     }
 
-    const prompt_tokens = encoding.encode(promptToSend);
+    // const prompt_tokens = encoding.encode(promptToSend);
 
-    let tokenCount = prompt_tokens.length;
+    // let tokenCount = prompt_tokens.length;
     let messagesToSend: Message[] = [];
 
     for (let i = messages.length - 1; i >= 0; i--) {
       const message = messages[i];
-      const tokens = encoding.encode(message.content);
+      // const tokens = encoding.encode(message.content);
 
-      if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
-        break;
-      }
-      tokenCount += tokens.length;
+      // if (tokenCount + tokens.length + 1000 > model.tokenLimit) {
+      //   break;
+      // }
+      // tokenCount += tokens.length;
       messagesToSend = [message, ...messagesToSend];
     }
 
