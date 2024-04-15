3758de5b4b8061b536c9226b9c359a2aa418b4c5
leason-wan <fadeneow@gmail.com>
2024-04-15 12:26:07 +0800

    feat: add baidu tongji

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,19 @@ type Props = DocumentProps & {
   // add custom document props
 };
 
+const getAnalyticsTag = () => {
+  return {
+    __html: `
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?你的代码";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();`,
+  }
+}
+
 export default function Document(props: Props) {
   const currentLocale =
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
@@ -13,6 +26,7 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        <script dangerouslySetInnerHTML={getAnalyticsTag()}/>
       </Head>
       <body>
         <Main />
